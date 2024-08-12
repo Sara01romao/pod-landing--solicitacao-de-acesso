11bfb35ea573b7fc0cd90358a69511f4f5f4fbaf
Sara Romao <sara-romao@live.com>
2024-08-12 18:19:39 -0300

    feat: msg eviu e erro

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById('enviar').addEventListener('click', (evt) => {
+    const email = document.querySelector('.email').value;
+    let msgCampo = document.querySelector('.msg-campo')
+    evt.preventDefault();
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "" || !emailRegex.test(email)) {
+       
+        msgCampo.classList.add('active');
+
+    } else {
+        msgCampo.classList.remove('active');
+        msgCampo.classList.add('enviado');
+        msgCampo.textContent = 'Email enviado!';
+        document.querySelector('.email').value ="";
+
+        setTimeout(() => {
+           
+            msgCampo.classList.remove('enviado')
+        }, 1000);
+    }
+});
